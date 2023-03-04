f3ebf664c1d454f4e8f8fb25481db21b689a07de
Prajval Gahine <77929912+p-n-g@users.noreply.github.com>
2023-03-04 10:06:51 +0530

    Update StudentScreener.cpp

--- practice-problems/skeletons-cpp/StudentScreener.cpp
+++ practice-problems/skeletons-cpp/StudentScreener.cpp
@@ -2,7 +2,36 @@
 using namespace std;
 
 string getEligibleStudents(int totalStudents,vector<string> students,vector<int>& marks,vector<string>& examResults,int totalMarks,float requiredPercentile){
-    //TODO: Implement this function and return the required output
+    map<int, pair<string, string>> info;
+    vector<string> ans;
+    string output = "";
+
+    for (int i = 0; i < totalStudents; i++)
+    {
+        info.insert(pair<int, pair<string, string>>(marks[i], pair<string, string>(students[i], examResults[i])));
+    }
+    int rank = 0;
+    for (auto val : info)
+    {
+        float percentile = float(rank) / totalStudents * 100.0;
+        // cout << val.second.first << " " << val.first << " " << percentile << " " << val.second.second << endl;
+        if (percentile >= requiredPercentile && val.second.second == "Passed")
+        {
+            ans.push_back(val.second.first);
+        }
+        rank++;
+    }
+
+    reverse(ans.begin(), ans.end());
+    for (auto elgStd : ans)
+    {
+        output += elgStd + ",";
+    }
+    if (output != "")
+    {
+        output.pop_back();
+    }
+    return output;
 }
 
 int main(){
